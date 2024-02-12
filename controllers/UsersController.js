0d8c2997bcb974a36128608c35ccaacdd77f7224
ManNjoro <elijohnmwoho@gmail.com>
2024-02-12 15:39:21 +0300

    Implement user authentication and user retrieval.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,17 +1,16 @@
-/* eslint-disable import/no-named-as-default */
 import sha1 from 'sha1';
 import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class UsersController {
-  static async postNew(req, res) {
-    const { email, password } = req.body;
+  static async postNew(request, response) {
+    const { email, password } = request.body;
     if (!email) {
-      res.status(400).json({ error: 'Missing email' });
+      response.status(400).json({ error: 'Missing email' });
     }
     if (!password) {
-      res.status(400).json({ error: 'Missing password' });
+      response.status(400).json({ error: 'Missing password' });
     }
 
     const hashPwd = sha1(password);
@@ -21,34 +20,34 @@ class UsersController {
       const user1 = await collection.findOne({ email });
 
       if (user1) {
-        res.status(400).json({ error: 'Already exist' });
+        response.status(400).json({ error: 'Already exist' });
       } else {
         collection.insertOne({ email, password: hashPwd });
         const newUser = await collection.findOne(
           { email }, { projection: { email: 1 } },
         );
-        res.status(201).json({ id: newUser._id, email: newUser.email });
+        response.status(201).json({ id: newUser._id, email: newUser.email });
       }
     } catch (error) {
       console.log(error);
-      res.status(500).json({ error: 'Server error' });
+      response.status(500).json({ error: 'Server error' });
     }
   }
 
-  static async getMe(req, res) {
+  static async getMe(request, response) {
     try {
-      const userToken = req.header('X-Token');
+      const userToken = request.header('X-Token');
       const authKey = `auth_${userToken}`;
       const userID = await redisClient.get(authKey);
       console.log('USER KEY GET ME', userID);
       if (!userID) {
-        res.status(401).json({ error: 'Unauthorized' });
+        response.status(401).json({ error: 'Unauthorized' });
       }
       const user = await dbClient.getUser({ _id: ObjectId(userID) });
-      res.json({ id: user._id, email: user.email });
+      response.json({ id: user._id, email: user.email });
     } catch (error) {
       console.log(error);
-      res.status(500).json({ error: 'Server error' });
+      response.status(500).json({ error: 'Server error' });
     }
   }
 }
