70a60d5c7693b7599200aec95f570ccae1ee68eb
ManNjoro <elijohnmwoho@gmail.com>
2024-02-12 14:59:45 +0300

    Add Express middleware to parse JSON bodies and use the route controller.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-import { express } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
 
 const router = express.Router();
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import routeController from './routes/index';
 
 const app = express();
+app.use(express.json());
+routeController(app);
 
 const PORT = process.env.PORT || 5000;
 
