fe4ea5f40ee4d7b522f62e19ba5ad527efd43ac7
ManNjoro <elijohnmwoho@gmail.com>
2024-02-14 21:22:39 +0300

    Remove unused function `isValidId` from `essentials.js`

--- utils/essentials.js
+++ utils/essentials.js
@@ -1,14 +1,6 @@
 import { ObjectId } from 'mongodb';
 
-/**
- * Module with basic utilities
- */
 const essentials = {
-  /**
-   * Checks if Id is Valid for Mongo
-   * @id {string|number} id to be evaluated
-   * @return {boolean} true if valid, false if not
-   */
   isValidId(id) {
     try {
       ObjectId(id);
